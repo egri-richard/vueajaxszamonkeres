2ec2b6db0c58562cb39e03bab670e7aee68e8d4a
egri-richard <egri.dani.r@gmail.com>
2022-02-08 14:21:16 +0100

    basic layout and list of statues

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Statues</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Személy</th>
+          <th>Magasság</th>
+          <th>Ár</th>
+          <th>Műveletek</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="s in statues" :key="s.id">
+          <td>{{ s.person }}</td>
+          <td>{{ s.height }}</td>
+          <td>{{ s.price }}</td>
+          <td><button>Szerkesztéd</button></td>
+          <td><button>Törlés</button></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      statues: Array
+    }
+  },
+  methods: {
+    async getStatues() {
+      const response = await fetch("http://127.0.0.1:8000/api/statues")
+      const data = await response.json()
+      this.statues = data
+    }
+  },
+  mounted() {
+    this.getStatues()
   }
 }
 </script>
