66f29f41b70a5d602c542638feeaa827bb195c57
egri-richard <egri.dani.r@gmail.com>
2022-02-08 16:02:16 +0100

    router implementation and design change

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,60 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <h1>Szobrok</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Személy</th>
+          <th>Magasság</th>
+          <th>Ár</th>
+          <th>Műveletek</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="s in statues" :key="s.id">
+          <td>{{ s.person }}</td>
+          <td>{{ s.height }}</td>
+          <td>{{ s.price }}</td>
+          <td>
+            <router-link to="/editStatue">Szerkesztés</router-link>
+          </td>
+          <td>
+            <button>Törlés</button>
+          </td>
+        </tr>
+        <tr>
+          <td colspan="3">
+            <router-link to="/createStatue">Új Szobor</router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      statues: Array
+    }
+  },
+  methods: {
+    async getStatues() {
+      const response = await fetch("http://127.0.0.1:8000/api/statues")
+      const data = await response.json()
+      this.statues = data
+    }
+  },
+  mounted() {
+    this.getStatues()
+  }
+}
+</script>
 
 <style>
 #app {
@@ -15,18 +63,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  margin-top: 60px;
 }
 </style>
