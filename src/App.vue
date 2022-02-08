e9a200669cd08eabacd73dc4b786d0e77439964e
egri-richard <egri.dani.r@gmail.com>
2022-02-08 15:50:34 +0100

    added router

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,12 @@
 <template>
   <div id="app">
-    <h1>Statues</h1>
-    <table>
-      <thead>
-        <tr>
-          <th>Személy</th>
-          <th>Magasság</th>
-          <th>Ár</th>
-          <th>Műveletek</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="s in statues" :key="s.id">
-          <td>{{ s.person }}</td>
-          <td>{{ s.height }}</td>
-          <td>{{ s.price }}</td>
-          <td><button>Szerkesztéd</button></td>
-          <td><button>Törlés</button></td>
-        </tr>
-      </tbody>
-    </table>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-
-
-export default {
-  name: 'App',
-  components: {},
-  data() {
-    return {
-      statues: Array
-    }
-  },
-  methods: {
-    async getStatues() {
-      const response = await fetch("http://127.0.0.1:8000/api/statues")
-      const data = await response.json()
-      this.statues = data
-    }
-  },
-  mounted() {
-    this.getStatues()
-  }
-}
-</script>
 
 <style>
 #app {
@@ -54,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
